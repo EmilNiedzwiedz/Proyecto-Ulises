e9edcc544799d82c57cac042efa759745caa2a7b
Emil <epniedzwiedz01@educarex.es>
2023-11-24 13:05:06 +0100

    Saludo a Don José

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Don Pepito");
+		
+		System.out.println("Don José");
 
 	}
 
